26752a3634f45cb4cdc7e4cd32856863a9e2bd63
Allan-Binga <allanbinga73@gmail.com>
2023-06-20 14:07:27 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import './App.css';
+
+function App() {
+  const[email, setEmail] = useState("");
+  const[message, setMessage] = useState("");
+  const emailValidation=()=>{
+    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+    if(regEx.test(email)){
+      setMessage("Email is valid");
+    }else if(!regEx.test(email)&& email !==""){
+      setMessage("Email is not valid!");
+    }else{
+      setMessage("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+  return(
+    <div className="app">
+      <div className="header">
+        <h2>Email Validation</h2>
+      </div>
+      <div className="card">
+        <label htmlFor="email" className="label">Email:</label>
+        <input id="email" className="input" type="email" placeholder="Email" value={email} onChange={handleOnChange}/>
+        <button onClick={emailValidation}>Check Email</button>
+      <p className="message">{message}</p>
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
